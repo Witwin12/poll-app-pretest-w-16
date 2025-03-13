89537698da37e1ee2d25717d206389f886b3610f
wit_win <kunwin12@gmail.com>
2025-03-13 14:32:16 +0700

    make models for pirvate polls

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Question,Choice
+from .models import Question,Choice,Pirvate_Choice,Pirvate_Question
 
 admin.site.register(Question)
 admin.site.register(Choice)
+admin.site.register(Pirvate_Question)
+admin.site.register(Pirvate_Choice)
--- polls/models.py
+++ polls/models.py
@@ -20,4 +20,19 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
+
+class Pirvate_Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+    def __str__(self):
+        return self.question_text
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
     
+class Pirvate_Choice(models.Model):
+    question = models.ForeignKey(Pirvate_Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
